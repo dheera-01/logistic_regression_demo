a71a16ccf7b5fd43772945f0fb0f5a9c17ede16b
Sandeep Desai <desai.sandeep@iitgn.ac.in>
2023-04-26 16:10:13 +0530

    all degree combo

--- demo.py
+++ demo.py
@@ -14,28 +14,76 @@ import streamlit as slt
 
 class logistic_regression():
     model=None
+    labels_degree={}
     def __init__(self, degree, L2_penalty):
         self.degree = degree
         self.L2_penalty = L2_penalty
     
+    def produce_combo(self,deg,n):
+        lst=[]
+        def dp(ind,deg,temp):
+            if(ind==n):
+                temp_copy=temp.copy()
+                
+                lst.append(temp_copy)
+                return
+
+            for i in range(0,deg+1):
+                temp.append(i)
+                dp(ind+1,deg-i,temp)
+                temp.pop()
+            return
+        dp(0,deg,[])
+        return lst
+
+    def transform(self,X):
+        """
+        Transform data to polynomial features
+        Generate a new feature matrix consisting of all polynomial combinations of the features with degree less than or equal to the specified degree. 
+        For example, if an input sample is  np.array([a, b]), the degree-2 polynomial features with "include_bias=True" are [1, a, b, a^2, ab, b^2].
+        
+        Inputs:
+        param X : (np.array) Dataset to be transformed
+        
+        Outputs:
+        returns (np.array) Tranformed dataset.
+        """
+        
+        lst=self.produce_combo(self.degree,X.shape[1])
+        
+        X_new=np.ones((X.shape[0],len(lst)))
+        for i in range(0,len(lst)):
+            text=""
+            for j in range(0,X.shape[1]):
+                text+="x"+str(j)+"^"+str(lst[i][j])+"*"
+                X_new[:,i]*=X[:,j]**lst[i][j]
+            text=text[:-1]
+            self.labels_degree[i]=text
+        
+        return X_new
+
     def fit(self,X,y):
         
         self.model = LogisticRegression(penalty='l2', C=self.L2_penalty)
         
         # add polynomial features
-        X_old=X
-        for i in range(2,degree+1):
-            X=np.concatenate((X,X_old**i),axis=1)
+        X_new=self.transform(X)
+        # X_old=X
+        # for i in range(2,degree+1):
+        #     X=np.concatenate((X,X_old**i),axis=1)
         
-        self.model.fit(X, y)
+        self.model.fit(X_new, y)
 
         
     
     def predict(self,X):
-        X_old=X
-        for i in range(2,degree+1):
-            X=np.concatenate((X,X_old**i),axis=1)
-        return self.model.predict(X)
+
+        
+        X_new=self.transform(X)
+        # X_old=X
+        # for i in range(2,degree+1):
+        #     X=np.concatenate((X,X_old**i),axis=1)
+        return self.model.predict(X_new)
         
     def plot_decision_surface(self,X):
         x1_min = np.min(X[:,0])
@@ -59,7 +107,10 @@ class logistic_regression():
 
     def plot_hist_weights(self):
         fig,ax=plt.subplots()
-        plt.hist(self.model.coef_[0])
+        # add labels as with degree
+        print(len(self.model.coef_[0]))
+        plt.bar(range(0,len(self.model.coef_[0])),abs(self.model.coef_[0]))
+        plt.xticks(range(0,len(self.model.coef_[0])),list(self.labels_degree.values()),rotation=90)
         slt.pyplot(fig)
 
    
